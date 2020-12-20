836fa56e55ba9fa08cd174da29e2c2bbc3193879
Oscar Daniel <t0d0c4m614@hotmail.com>
2020-12-20 14:25:50 -0500

    Updates

---
+++ precache-manifest.9641bedecf8a596173d4f201c69fd647.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "743154ed775b650d91d90ea59970bb0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e76a03dbedec5b5dc386",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "98e4448e4c3ce11a5bff",
+    "url": "/static/css/main.4ab32467.chunk.css"
+  },
+  {
+    "revision": "e76a03dbedec5b5dc386",
+    "url": "/static/js/2.4fab7bc0.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.4fab7bc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98e4448e4c3ce11a5bff",
+    "url": "/static/js/main.51b96d57.chunk.js"
+  },
+  {
+    "revision": "303c68f0820dd56a25b6",
+    "url": "/static/js/runtime-main.f5279e05.js"
+  },
+  {
+    "revision": "2353a87e57174c1c5db6e5a0b07679b2",
+    "url": "/static/media/icomoon.2353a87e.ttf"
+  },
+  {
+    "revision": "790b9b5e948f91d5aecdec947e1202c9",
+    "url": "/static/media/icomoon.790b9b5e.svg"
+  },
+  {
+    "revision": "82f142a9fb659bcfc6058e4e1dca4ef2",
+    "url": "/static/media/icomoon.82f142a9.eot"
+  },
+  {
+    "revision": "a1debf118390e4d94ae52b0fc6c36caf",
+    "url": "/static/media/icomoon.a1debf11.woff"
+  }
+]);
